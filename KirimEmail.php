200bd2240d019cb5e1bdae0a34107d5451930e50
LENTTT <144861856+LENTTT@users.noreply.github.com>
2025-03-03 08:42:19 +0700

    Add files via upload
    fisrt

---
+++ KirimEmail.php
@@ -0,0 +1,26 @@
+<?php
+// Recipient email address
+$to = 'zaid@zsecurity.org';
+
+// Subject of the email
+$subject = 'Test';
+
+// Message body
+$message = 'This is another email spoofing test.';
+
+// Sender email and name
+$senderEmail = 'valentinus.wilbert@student.id';
+$senderName = 'Valentinus Wilbert';
+
+// Additional headers
+$headers = 'From: ' . $senderName . ' <' . $senderEmail . '>' . "\r\n" .
+           'Reply-To: ' . $senderEmail . "\r\n" .
+           'X-Mailer: PHP/' . phpversion();
+
+// Send email
+if (mail($to, $subject, $message, $headers)) {
+    echo 'Email sent successfully!';
+} else {
+    echo 'Failed to send email.';
+}
+?>
